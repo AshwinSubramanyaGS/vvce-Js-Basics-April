325ff806b7ce133a33826bde0a8867c1adfd42ac
AshwinSubramanyaGS <ashwin.subramanya@gmail.com>
2025-04-22 14:33:46 +0530

    external

---
+++ script.js
@@ -0,0 +1,4 @@
+alert("Welcome to our website!");
+        const userName = prompt("What's your name?");
+        confirm(`Is ${userName} correct username?`);
+        console.log("User entered:", userName);
