7d3ce1b3c7c233fcc2328545375f7d947653cf96
AshwinSubramanyaGS <ashwin.subramanya@gmail.com>
2025-04-22 15:30:44 +0530

    Map

--- script.js
+++
@@ -1,4 +0,0 @@
-alert("Welcome to our website!");
-        const userName = prompt("What's your name?");
-        confirm(`Is ${userName} correct username?`);
-        console.log("User entered:", userName);
